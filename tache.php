ae5ae42a8b9119722d7c09b5e963c428fa7409b4
VincentMart <vincent.martin.2002@gmail.com>
2021-11-24 11:58:59 +0100

    ajout des classes objets et gateways

---
+++ tache.php
@@ -0,0 +1,16 @@
+<?php
+
+//recuperer le nom de l'utilisateur
+
+class Tache{
+
+    private $id;
+    private $texte;
+    private $status;
+
+    public function __construct(){
+        this->id=$id;
+        this->texte= $texte;
+        this->status= $status;
+    }
+}
---
+++ utilisateur.php
@@ -0,0 +1,14 @@
+<?php
+
+class Utilisateur{
+
+    private $user_id;
+    private $user_name;
+    private $password;
+
+    public function __construct(){
+        this->user_id=$user_id;
+        this->user_name= $user_name;
+        this->password= $password;
+    }
+}
