22bf429b799d45b5dcb3e2cfb959e7df16df29f0
VincentMart <92922812+VincentMart@users.noreply.github.com>
2021-12-16 10:09:47 +0100

    membre controller

--- controller/FrontController.php
+++ controller/FrontController.php
@@ -1,6 +1,20 @@
 <?php
 
-public class FrontController
-{
+public class FrontController{
+    public function __construct(){
+        global;
+    }
 
+    public function isMembre(){
+    }
+
+    public function connection(){
+
+    }
+
+    public function deconnection(){
+        session_destroy();
+    }
 }
+
+?>
