392e1f25d7ed210f19f078c511f2727cffc2051c
VincentMart <vincent.martin.2002@gmail.com>
2021-12-30 16:12:37 +0100

    commit front controller

--- controller/FrontController.php
+++ controller/FrontController.php
@@ -2,19 +2,39 @@
 
 public class FrontController{
     public function __construct(){
-        global;
-    }
+        global $rep, $vue, $action;
 
-    public function isMembre(){
-    }
+        $listeAction = array('connexion','deconnexion', 'ajouterListe','ajouterTache','supprimerListe','supprimerTache');
+        session_start();
+        $dVueErreur = array();
 
-    public function connection(){
+        try{
+            $isMembre = new MembreModel();
 
-    }
+            if(!empty($_REQUEST['action'])){
+                $action = $_REQUEST['action'];
+            }
 
-    public function deconnection(){
-        session_destroy();
+            if(in_array($action, $listeAction)){
+                if($action == "connexion" && !empty($_REQUEST['login']) && !empty($_REQUEST['password'])){
+                    $isMembre->connexion($_REQUEST['login']), $_REQUEST['password']);
+                } elseif($action == "deconnexion") {
+                    $isMembre->deconnexion();
+                }
+
+                if($isMembre->isMembre() == NULL){
+                    require('vue/connexion.php');
+                } else {
+                    $membre = new MembreController();
+                }
+            } else {
+                $user = new UtilisateurController();
+            }
+        }
+        catch (Exception $e){
+            $dVueErreur[] = "Erreur : ".$e->getMessage()."<br/>";
+            require($rep.$vue['erreur']);
+        }
     }
 }
-
 ?>
