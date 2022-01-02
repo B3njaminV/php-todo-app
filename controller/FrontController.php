38159406051d7e56f5e7926ecee9bbbfb58832b7
VincentMart <92922812+VincentMart@users.noreply.github.com>
2022-01-02 15:22:01 +0100

    commit debut du front

--- controller/FrontController.php
+++ controller/FrontController.php
@@ -1,12 +1,14 @@
 <?php
 
+    namespace controller;
+    require "MembreController.php";
+    require "UtilisateurController.php";
+
     public class FrontController{
         public function __construct(){
-            global $rep, $vue, $action;
-            $listeActionCToutCassé = array();
+            global $rep, $vue;
             $listeActionMembre = array('connexion','deconnexion','checkPublic','checkPrive','ajouterListePublic','ajouterListePrive', 'ajouterTachePrive','ajouterTachePublic','supprimerListePublic','supprimerListePrive','supprimerTachePrive','supprimerTachePublic');
             $listeActionUser = array('ajouterListePublic','ajouterTachePublic','checkPublic');
-            session_start();
             $dVueErreur = array();
 
             try{
@@ -17,40 +19,11 @@
                     $action = $_REQUEST['action'];
                 }
 
-                if(in_array($action, $listeActionCToutCassé)){
+                if(in_array($action)){
                     if($action == "connexion" && !empty($_REQUEST['login']) && !empty($_REQUEST['password'])){
                         $isMembre->connexion($_REQUEST['login']), $_REQUEST['password']);
                     } elseif($action == "deconnexion") {
                         $isMembre->deconnexion();
-                    }
-
-                    if($action == "ajouterListePrive" && !empty($_REQUEST['login']) && !empty($_REQUEST['password'])) {
-                        $isMembre->ajouter_liste_prive();
-                    } else {
-                        require('vue/connexion.php');
-                    }
-
-                    if($action == "ajouterTachePrive" && !empty($_REQUEST['login']) && !empty($_REQUEST['password'])) {
-                        $isMembre->ajout_tache_prive();
-                    } else {
-                        require('vue/connexion.php');
-                    }
-
-                    if($action == "checkPrive"){
-                        $is
-                    }
-
-                    if($action == "checkPublic"){
-
-                    }
-
-                    //je me souviens plus si un membre peut ajouter des taches publiques
-                    if($action == "ajouterListePublic"){
-                        $isMembre->ajout_liste_public();
-                    }
-
-                    if($action == "ajouterTachePublic"){
-                        $isMembre->ajout_tache_public();
                     }
 
                     if($isMembre->isMembre() == NULL){
